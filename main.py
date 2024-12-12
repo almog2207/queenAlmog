6fae726babd271cac76666c88644e1f2833c108b
almog <almog220708@gmai.com>
2024-12-12 18:46:02 +0200

    היי אור זה פונקציב שמקבלת מספר ובודקת אם הוא מספר ראשוני אם כן מחזירה TRUE אחרת מחזירה FALSE  חח

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+def firstNum(num1):
+    for i in range(2,num1):
+        num = 0
+        if num1 % i == 0:
+            return False
+    return True
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print(firstNum(5))
+
+print()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
